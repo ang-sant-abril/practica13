51b801e20c0082f33808d7990c3251fae78e6a8c
ang-sant-abril <ang-sant-abril@gmail.com>
2021-04-15 11:31:10 +0000

    initial

---
+++ src/app/facturas/listado-facturas/listado-facturas.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FacturasService } from 'src/app/servicios/facturas.service';
+
+@Component({
+  selector: 'app-listado-facturas',
+  templateUrl: './listado-facturas.component.html',
+  styleUrls: ['./listado-facturas.component.css']
+})
+export class ListadoFacturasComponent implements OnInit {
+
+  facturas: any;
+  cargando: boolean = true;
+
+  constructor(private facturasService: FacturasService) { }
+
+  ngOnInit(): void {
+    this.facturasService.getFacturas()
+                        .subscribe((resp: any) => {
+                          this.cargando = false;
+                          this.facturas = resp.facturas;
+                        }, (err: any) => {
+                          this.cargando = false;
+                          console.log(err);
+                        })
+  }
+
+}
